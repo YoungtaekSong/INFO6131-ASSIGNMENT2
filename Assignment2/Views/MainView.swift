b63bc566547138969300a7c6e065dd1a55aeca10
Andrew Youngtaek Song <y_song179344@fanshaweonline.ca>
2024-08-15 22:36:06 -0400

    Added Tabbar

---
+++ Assignment2/Views/MainView.swift
@@ -0,0 +1,37 @@
+//
+//  MainView.swift
+//  Assignment2
+//
+//  Created by Andrew Song on 2024-08-15.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @State private var selectedTab = 1
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            VStack {
+                EmptyView()
+            }
+            .tabItem {
+                Label("Employee", systemImage: "person.3")
+            }
+            VStack {
+                Text("Settings").font(.title)
+                Spacer()
+                Text("Young Taek Song")
+                Text("1122390")
+                Spacer()
+            }
+            .tabItem {
+                Label("Settings", systemImage: "gear")
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
